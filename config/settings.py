5d9e1acf3ab2ad48eb1653a2a361c083caf2a5cb
00016014 <00016014wiut@gmail.com>
2026-03-05 22:31:32 +0500

    Add CSRF trusted origins for production

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-123')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
